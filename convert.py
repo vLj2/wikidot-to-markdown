70caf2e4647cfbb774a978da3f9521c3a99e4f17
Philipp Klaus <philipp.klaus@gmail.com>
2012-06-14 16:03:19 +0200

    Split Code Logically
    * Wikidot-to-Markdown Converter Class
    * Command Line Tool
      Makes use of the class, converts content and saves it to a folder.

--- convert.py
+++ convert.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python
 
-## for string.join()
-import string
+from wikidot import WikidotToMarkdown ## most important here
 
-import sys
-
-## for os.makedirs()
-import os
-
-## for optparse.OptionParser()
-import optparse
-
-import re
-
-import markdown
-
-## for codecs.open()
-import codecs
-
-## to generate random UUIDs using uuid.uuid4()
-import uuid
+import sys ## for sys.exit()
+import os ## for os.makedirs()
+import optparse ## for optparse.OptionParser()
+import markdown ## for markdown.markdown()
+import codecs ## for codecs.open()
+import datetime as dt # for dt.datetime() and dt.datetime.now()
+import time ## for time.sleep()
 
 # https://github.com/maxcutler/python-wordpress-xmlrpc
 from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.methods.posts import GetRecentPosts, NewPost
+from wordpress_xmlrpc.methods.posts import NewPost
 from wordpress_xmlrpc.methods.users import GetUserInfo
-
-# for dt.datetime() and dt.datetime.now()
-import datetime as dt
-
-## for time.sleep()
-import time
 
 SITE = 'http://blog.example.com/xmlrpc.php'
 #SITE = 'http://yourblog.wordpress.com/xmlrpc.php'
@@ -39,85 +22,27 @@ DEFAULT_OUTPUT_DIR = "output"
 #SLEEP_TIME = 1 # seconds to sleep after each post sent to the blog (if you use your own server, set this to 0)
 SLEEP_TIME = 0
 
-class WikidotToMarkdown(object):
-    def __init__(self):
-        # regex for URL found on http://regexlib.com/REDetails.aspx?regex_id=501
-        self.url_regex = r"(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*[/]?"
-
-        self.static_replacements = { '[[toc]]': '', # no equivalent for table of contents in Markdown
-                                   }
-        self.regex_replacements = { r'^\+ ([^\n]*)$': r"# \1\n", # headings
-                                     r'^\+\+ ([^\n]*)$': r"## \1\n",
-                                     r'^\+\+\+ ([^\n]*)$': r"### \1\n",
-                                     r'([^:])//([\s\S ]*?)//': r'\1*\2*',
-                                   }
-        self.regex_split_condition = r"^\+ ([^\n]*)$"
-    
-    def convert(self, text):
-        # first we search for [[code]] statements as we do not any replacement to happen inside those code blocks!
-        code_blocks = dict()
-        code_blocks_found = re.findall(re.compile(r'(\[\[code( type="([\S]+)")?\]\]([\s\S ]*?)\[\[/code\]\])',re.MULTILINE), text)
-        for code_block_found in code_blocks_found:
-            tmp_hash = str(uuid.uuid4())
-            text = text.replace(code_block_found[0],tmp_hash,1) # replace code block with a hash - to fill it in later
-            code_blocks[tmp_hash] = "\n"+string.join(["    " + l for l in code_block_found[-1].strip().split("\n") ],"\n")+"\n"
-        for search, replacement in self.static_replacements.items():
-            text = text.replace(search,replacement,1)
-        # search for any of the simpler replacements in the dictionary regex_replacements
-        for s_reg, r_reg in self.regex_replacements.items():
-            text = re.sub(re.compile(s_reg,re.MULTILINE),r_reg,text)
-        # search for simple http://www.google.com links:
-        for link in re.finditer(r"[\s\S\n ]("+self.url_regex+r")", text):
-            if link.group(0)[0] == "[" : continue
-            text = text.replace(link.group(1),"<%s>  " % link.group(1),1)
-        # search for links of the form [http://www.google.com Google Website]
-        for link in re.finditer(r"\[("+self.url_regex+r") ([^\]]*)\]", text):
-            #print link.group(0), "[%s](%s)" % (link.groups()[-1],link.group(1))
-            text = text.replace(link.group(0),"[%s](%s)" % (link.groups()[-1],link.group(1)),1)
-    # search for unhandled tags and state them
-        for unhandled_tag in re.finditer(r"\[\[/([\s\S ]*?)\]\]", text):
-            print("Found an unhandled tag: %s" % unhandled_tag.group(1))
-        # now we substitute back our code blocks
-        for tmp_hash, code in code_blocks.items():
-            text = text.replace(tmp_hash, code, 1)
-        return text
-
-    def split_text(self, text):
-        output_parts = []
-        split_regex = re.compile(self.regex_split_condition) 
-        for line in text.split("\n"):
-            line += "\n"
-            if len(output_parts) > 0 and (re.match(split_regex,line) == None): output_parts[-1] += line
-            else: output_parts.append(line)
-        return output_parts
-            
 class ConversionController(object):
-    """"""
-    
     def __init__(self, options):
-        """constructor
-        """
         self.__input_wiki_file = options.filename
         self.__output_directory = options.output_dir
         self.__fill_blog = options.blog
         self.__create_individual_files = options.individual
-
         self.__converter = WikidotToMarkdown()
 
     def __prepare_output_dir(self):
-        #os.makedirs(self.__output_directory) ## to see the exception do not put it inside a try..except
         try:
             os.makedirs(self.__output_directory)
         except OSError as ex:
             print("Could not create output folder "+self.__output_directory+".")
             if ex.errno == os.errno.EEXIST: print("It already exists.")
             else: print "Error %i: %s" % (ex.errno, str(ex)); sys.exit(1)
-    
+
     def convert(self):
         self.__prepare_output_dir()
         f = codecs.open(self.__input_wiki_file, encoding='utf-8')
         text = f.read()
-        
+
         # write the complete files to the output directory:
         complete_text = self.__converter.convert(text)
         self.write_unicode_file("%s/%s" % (self.__output_directory, 'complete.mktxt'),complete_text)
@@ -134,10 +59,10 @@ class ConversionController(object):
             if self.__fill_blog:
                 wprb = WordPressPostingRobot(SITE,USER)
                 start_day = raw_input('Please enter the start date for the posts: [%s] ' % dt.datetime.now().strftime("%Y-%m-%d") )
-            start_day = start_day if start_day != "" else dt.datetime.now().strftime("%Y-%m-%d")
+                start_day = start_day if start_day != "" else dt.datetime.now().strftime("%Y-%m-%d")
                 start = [int(value) for value in start_day.split("-")]
                 end_day = raw_input('Please enter the end date for the posts: [%s] ' % dt.datetime.now().strftime("%Y-%m-%d") )
-            end_day = end_day if end_day != "" else dt.datetime.now().strftime("%Y-%m-%d")
+                end_day = end_day if end_day != "" else dt.datetime.now().strftime("%Y-%m-%d")
                 end = [int(value) for value in end_day.split("-")]
                 days_difference = (dt.datetime(end[0],end[1],end[2])-dt.datetime(start[0],start[1],start[2])).days
                 gradient = .0 if len(parts) == 2 else float(days_difference)/(len(parts)-2)
@@ -150,10 +75,10 @@ class ConversionController(object):
                 if self.__create_individual_files: self.write_unicode_file("%s/%i%s" % (self.__output_directory, i, '.mktxt'),text_part)
                 lines = text_part.split("\n")
                 if self.__fill_blog:
-                title = lines[0].replace("# ","")
-                content = string.join(lines[1:],'\n')
-                date = dt.datetime(start[0],start[1],start[2], 17, 11, 11) + dt.timedelta(int((i-2)*gradient))
-                wprb.post_new(title, content,[],'','private',date)
+                    title = lines[0].replace("# ","")
+                    content = string.join(lines[1:],'\n')
+                    date = dt.datetime(start[0],start[1],start[2], 17, 11, 11) + dt.timedelta(int((i-2)*gradient))
+                    wprb.post_new(title, content,[],'','private',date)
                     time.sleep(SLEEP_TIME)
 
     def write_unicode_file(self, path_to_file, content):
@@ -169,7 +94,6 @@ class WordPressPostingRobot(object):
         if password == "": password = raw_input('Please enter the password for the user %s: ' % user)
         self.additional_tags = raw_input('Please enter additional tags to give to all the posts: ')
         self.__wp = Client(site, user, password)
-        #wp.call(GetRecentPosts(10))
         #wp.call(GetUserInfo())
 
     def post_new(self, title, content, categories = ['Mac OS X'], individual_tags = '', status = 'private', date = dt.datetime.now()):
@@ -186,15 +110,15 @@ class WordPressPostingRobot(object):
 
 
 def main():
-    """ Main function called to start the conversion.""" 
+    """ Main function called to start the conversion."""
     p = optparse.OptionParser(version="%prog 1.0")
-    
+
     # set possible CLI options
     p.add_option('--save-junks-to-blog', '-b', action="store_true", help="save the individual files as blog posts (only relevant if -s set)", default=False, dest="blog")
     p.add_option('--save-individual', '-s', action="store_true", help="save individual files for every headline", default=False, dest="individual")
     p.add_option('--input-file', '-f', metavar="INPUT_FILE", help="Read from INPUT_FILE.", dest="filename")
     p.add_option('--output-dir', '-o', metavar="OUTPUT_DIRECTORY", help="Save the converted files to the OUTPUT_DIRECTORY.", dest="output_dir")
-    
+
     # parse our CLI options
     options, arguments = p.parse_args()
     if options.filename == None:
---
+++ wikidot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import re ## The most important module here!
+import string ## for string.join()
+#import markdown
+import uuid ## to generate random UUIDs using uuid.uuid4()
+
+class WikidotToMarkdown(object):
+    def __init__(self):
+        # regex for URL found on http://regexlib.com/REDetails.aspx?regex_id=501
+        self.url_regex = r"(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*[/]?"
+
+        self.static_replacements = { '[[toc]]': '', # no equivalent for table of contents in Markdown
+                                   }
+        self.regex_replacements = { r'^\+ ([^\n]*)$': r"# \1\n", # headings
+                                     r'^\+\+ ([^\n]*)$': r"## \1\n",
+                                     r'^\+\+\+ ([^\n]*)$': r"### \1\n",
+                                     r'([^:])//([\s\S ]*?)//': r'\1*\2*',
+                                   }
+        self.regex_split_condition = r"^\+ ([^\n]*)$"
+
+    def convert(self, text):
+        # first we search for [[code]] statements as we do not any replacement to happen inside those code blocks!
+        code_blocks = dict()
+        code_blocks_found = re.findall(re.compile(r'(\[\[code( type="([\S]+)")?\]\]([\s\S ]*?)\[\[/code\]\])',re.MULTILINE), text)
+        for code_block_found in code_blocks_found:
+            tmp_hash = str(uuid.uuid4())
+            text = text.replace(code_block_found[0],tmp_hash,1) # replace code block with a hash - to fill it in later
+            code_blocks[tmp_hash] = "\n"+string.join(["    " + l for l in code_block_found[-1].strip().split("\n") ],"\n")+"\n"
+        for search, replacement in self.static_replacements.items():
+            text = text.replace(search,replacement,1)
+        # search for any of the simpler replacements in the dictionary regex_replacements
+        for s_reg, r_reg in self.regex_replacements.items():
+            text = re.sub(re.compile(s_reg,re.MULTILINE),r_reg,text)
+        # search for simple http://www.google.com links:
+        for link in re.finditer(r"[\s\S\n ]("+self.url_regex+r")", text):
+            if link.group(0)[0] == "[" : continue
+            text = text.replace(link.group(1),"<%s>  " % link.group(1),1)
+        # search for links of the form [http://www.google.com Google Website]
+        for link in re.finditer(r"\[("+self.url_regex+r") ([^\]]*)\]", text):
+            #print link.group(0), "[%s](%s)" % (link.groups()[-1],link.group(1))
+            text = text.replace(link.group(0),"[%s](%s)" % (link.groups()[-1],link.group(1)),1)
+        # search for unhandled tags and state them
+        for unhandled_tag in re.finditer(r"\[\[/([\s\S ]*?)\]\]", text):
+            print("Found an unhandled tag: %s" % unhandled_tag.group(1))
+        # now we substitute back our code blocks
+        for tmp_hash, code in code_blocks.items():
+            text = text.replace(tmp_hash, code, 1)
+        return text
+
+    def split_text(self, text):
+        output_parts = []
+        split_regex = re.compile(self.regex_split_condition)
+        for line in text.split("\n"):
+            line += "\n"
+            if len(output_parts) > 0 and (re.match(split_regex,line) == None): output_parts[-1] += line
+            else: output_parts.append(line)
+        return output_parts
